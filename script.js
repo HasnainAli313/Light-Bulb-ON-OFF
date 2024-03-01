d0d7e64b4b09139ca1ce6edd8b7c457d55bf479a
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-03-01 15:13:29 +0500

    completed project

---
+++ script.js
@@ -0,0 +1,12 @@
+// function for light ON
+function on(){
+     document.getElementById('bulb').src='./images/ONbulb.png';
+     document.body.style.backgroundColor="black";
+
+    }
+
+// function for light OFF
+function off(){
+  document.getElementById('bulb').src="./images/OFFbulb.png"
+  document.body.style.backgroundColor="white";
+}
